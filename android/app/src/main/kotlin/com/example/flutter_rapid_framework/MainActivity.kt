455013e740b46a8b454405dd96803ee257e090d6
yuanze-laolin <linxiang@yuanze.com>
2025-11-21 11:46:19 +0800

    Update：Modify the project package name

--- android/app/src/main/kotlin/com/example/flutter_rapid_framework/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_rapid_framework
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/lusun/teleprompter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lusun.teleprompter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
